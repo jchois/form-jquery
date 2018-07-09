3692c231528eca6ab4f2d8690b17e15b0b4b95db
juliana <choisjuliana@gmail.com>
2018-07-09 16:18:38 -0500

    add button

--- js/main.js
+++ js/main.js
@@ -16,9 +16,6 @@ $(document).ready(function(){
         
         valName();
         valFields();
-        valFields2();
-        valFields3();
-        valFields4();
     }
     function valName(){
 
@@ -41,147 +38,67 @@ $(document).ready(function(){
         var percent = $('#percentage1').val();
 
         if((perc1 == "") || (perc2 == "") || (percent == "")){
-            $('.xclose').css("display", "block");
+            $('.xclose').addClass("show");
             $('.xclose').html("<strong>Error!</strong> One or more of the fields is empty")
         }else{
         if(perc1 > perc2){
-            $('.xclose').css("display", "block");
+            $('.xclose').addClass("show");
             $('.xclose').html("<strong>Error!</strong> The second field of the ranges must be greater than the first")
         }else{
             $('.xclose').css("display", "none");
         }
         if(percent > 100){
-            $('.xclose').css("display", "block");
+            $('.xclose').addClass("show");
             $('.xclose').html("<strong>Error!</strong> The range of the percentage field must be between 1-100")
             // $('#percentage1').addClass("border-danger");
         }
         }
     }
-    function valFields2(){
-        var perc1 = $("#perc3").val();
-        var perc2 = $("#perc4").val();
-        var percent = $('#percentage2').val();
-
-        if((perc1 == "") || (perc2 == "") || (percent == "")){
-            $('.xclose2').css("display", "block");
-            $('.xclose2').html("<strong>Error!</strong> One or more of the fields is empty")
-        }else{
-        if(perc1 > perc2){
-            $('.xclose2').css("display", "block");
-            $('.xclose2').html("<strong>Error!</strong> The second field of the ranges must be greater than the first")
-        }else{
-            $('.xclose2').css("display", "none");
-        }
-        if(percent > 100){
-            $('.xclose2').css("display", "block");
-            $('.xclose2').html("<strong>Error!</strong> The range of the percentage field must be between 1-100")
-        }
-        }
-    }
-    function valFields3(){
-        var perc1 = $("#perc5").val();
-        var perc2 = $("#perc6").val();
-        var percent = $('#percentage3').val();
-
-        if((perc1 == "") || (perc2 == "") || (percent == "")){
-            $('.xclose3').css("display", "block");
-            $('.xclose3').html("<strong>Error!</strong> One or more of the fields is empty")
-        }else{
-        if(perc1 > perc2){
-            $('.xclose3').css("display", "block");
-            $('.xclose3').html("<strong>Error!</strong> The second field of the ranges must be greater than the first")
-        }else{
-            $('.xclose3').css("display", "none");
-        }
-        if(percent > 100){
-            $('.xclose3').css("display", "block");
-            $('.xclose3').html("<strong>Error!</strong> The range of the percentage field must be between 1-100")
-        }
-        }
-    }
-    function valFields4(){
-        var perc1 = $("#perc7").val();
-        var perc2 = $("#perc8").val();
-        var percent = $('#percentage4').val();
-
-        if((perc1 == "") || (perc2 == "") || (percent == "")){
-            $('.xclose4').css("display", "block");
-            $('.xclose4').html("<strong>Error!</strong> One or more of the fields is empty")
-        }else{
-        if(perc1 > perc2){
-            $('.xclose4').css("display", "block");
-            $('.xclose4').html("<strong>Error!</strong> The second field of the ranges must be greater than the first")
-        }else{
-            $('.xclose4').css("display", "none");
-        }
-        if(percent > 100){
-            $('.xclose4').css("display", "block");
-            $('.xclose4').html("<strong>Error!</strong> The range of the percentage field must be between 1-100")
-        }
-        }
-    }
     function yearsSelect(){
 
-        $('.years select').on('change', function() {
-            var years = $(".years select").val();
-            $(".years-inputs input").remove();
-            
-            for (let index = 1; index <= years; index++) {
-                $(".years-inputs").append(`<input type="text" class="form-control input-bott" placeholder="Year ${index}">`)
-            }
-        })
+        $(document.body).on( 'change', '.years-selector', function () {
+            var years = $(this).val();
+            var dataSectionId = $(this).attr("data-section");
 
-        $('.years2 select').on('change', function() {
-            var years = $(".years2 select").val();
-            $(".years-inputs2 input").remove();
+            $(`#${dataSectionId} .years-inputs input`).remove();
             
             for (let index = 1; index <= years; index++) {
-                $(".years-inputs2").append(`<input type="text" class="form-control input-bott" placeholder="Year ${index}">`)
-            }
-        })
-
-        $('.years3 select').on('change', function() {
-            var years = $(".years3 select").val();
-            $(".years-inputs3 input").remove();
-            
-            for (let index = 1; index <= years; index++) {
-                $(".years-inputs3").append(`<input type="text" class="form-control input-bott" placeholder="Year ${index}">`)
-            }
-        })
-
-        $('.years4 select').on('change', function() {
-            var years = $(".years4 select").val();
-            $(".years-inputs4 input").remove();
-            
-            for (let index = 1; index <= years; index++) {
-                $(".years-inputs4").append(`<input type="text" class="form-control input-bott" placeholder="Year ${index}">`)
+                $(`#${dataSectionId} .years-inputs`).append(`<input type="text" class="form-control input-bott" placeholder="Year ${index}">`)
             }
         })
     }
 
     function addBlock(){
+        var addDiv = $('.form');
+        var count = $(".form section").length;
+        var i = 1;
 
-        if(sectionPos >= cantSection){
-            sectionPos = 1;
-        }else{
-            sectionPos++;
+        if(count >= 1){
+            $('#remove').addClass("show");
         }
 
-        console.log(sectionPos);
-
-        $(".form section:nth-child(2)").fadeIn();
-       // $(".form section").css("display", "block");
-    }
-    function removeBlock(){
+        if(count <= 3){
+            $(`<section id="section${count+1}"> <div class="alert alert-danger xclose"></div><div class="info"><div class="data1"><label>Ranges</label><div class="range"><div class="input-group input-div"><input type="number" class="form-control" name="range1" id="perc1"><span id="span-pru" class="input-group-addon"><i class="fa fa-percent"></i></span></div><label class="to">TO</label><div class="input-group">      <input type="number" class="form-control" name="range2" id="perc2"><span id="span-pru" class="input-group-addon"><i class="fa fa-percent"></i></span></div></div></div><div class="data2"> <div class="years-por"><div class="years"><label for="years-select2">Years</label> <select data-section="section${count+1}" class="form-control years-selector" name="years2"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option></select></div><div class="years-inputs"><label for="percentage1">Percentage</label><input type="number" class="form-control input-bott" id="percentage1" placeholder="Year 1"></div></div></div></div></section>`).appendTo(addDiv);
+        }
         
-        if(sectionPos <= 1){
-            sectionPos = cantSection;
-        }else{
-            sectionPos--;
+        if(count == 3){
+            $("#add").addClass("hide");
+        }
+    }
+
+    function removeBlock(){
+        var count = $(".form section").length
+
+        if(count <= 4){
+            $('#add').addClass("show");
+        }
+        if(count >= 2){
+            $('#remove').addClass("hide");
         }
 
-        console.log(sectionPos);
+        $(".form section").last().remove();
     }
+
     function hideBlock(){
         $('.xclose').hide();
     }
